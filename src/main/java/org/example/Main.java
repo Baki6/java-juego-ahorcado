26c73082c619d4102cfe4b5b3675b3bcdf035c91
Victor Moctezuma <victorvmm10@gmail.com>
2024-09-08 16:03:58 -0600

    My First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String saludar = "Hola Mundo desde Java";
+        System.out.println("saludar = " + saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 10;
+
+        boolean valor = true;
+        int numero2 = 5;
+        if (valor) {
+            System.out.println("numero = " + numero);
+            
+        }
+    }
+}
